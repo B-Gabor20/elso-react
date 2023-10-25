d83e9f6a42b286a0f297b35588bf5be98bae18b4
B-Gabor20 <gaborbodnar92@gmail.com>
2023-10-25 13:19:42 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [isActive, setActive ] =useState(true);
+  return(
+    <div className='border'>
+    App
+    <BoxComponent isActive={isActive}/>
+    <ButtonComponent isActive={isActive} setActive={setActive}/>
+    <p>App színű state: {isActive? "Aktív" : "Inaktív"}</p>
     </div>
-  );
+  )  
 }
+function ButtonComponent({isActive, setActive }){
+  return (
+    <div
+      style={{
+        width: "200px",
+        height: "200px",
+      }}
+      className='p-2 m-5 rounded'
+      >
+        <button className='btn btn-success m-2'
+        disabled={isActive}
+        onClick={() => {
+          setActive(true);
+        }}
+        >Aktiválás</button>
 
+        <button 
+        className='btn btn-danger m-2'
+        disabled={!isActive}
+        onClick={() => {
+          setActive(false);
+        }}
+        >Deaktiválás</button>
+    </div>
+    
+  )
+}
+function BoxComponent(props){
+  return(
+    <div className="p-2 m-5 rounded"
+      style={{
+        width: "500px",
+        height: "200px",
+        backgroundColor: props.isActive ? "green" : "firebrick"
+    }}>
+      <h2>{props.isActive? "Aktív":"Inaktív"}</h2>
+    </div>
+
+  )
+}
 export default App;
